8388b3656d02f7bdf53056500aea26cd4cafd733
citec-47 <mauricendonyi40@gmail.com>
2023-03-20 06:39:59 -0700

    Editing of removal button

--- main.js
+++ main.js
@@ -7,71 +7,71 @@ class BookCollection {
     this.bookstore = JSON.parse(localStorage.getItem(this.BOOKS)) || [];
     this.myTites = document.getElementById('inputone');
     this.myAuth = document.getElementById('inputtwo');
-    this.myAdd = document.getElementById('butn');
-    this.myList = document.getElementById('list-of-books');
+    this.ourAdd = document.getElementById('butn');
+    this.List = document.getElementById('list-of-books');
   }
 
-  saveBooks() {
-    localStorage.setItem(this.BOOKS, JSON.stringify(this.bookstore ));
+  saveBookf() {
+    localStorage.setItem(this.BOOKS, JSON.stringify(this.bookstore));
   }
 
-  addBook() {
+  addBuks() {
     const title = this.myTites.value.trim();
-    const author = this.myAuth .value.trim();
+    const author = this.myAuth.value.trim();
 
     if (title && author) {
       const book = { title, author };
-      this.bookstore .push(book);
-      this.saveBooks();
-      this.renderBooks();
+      this.bookstore.push(book);
+      this.saveBookf();
+      this.deliverBooks();
       this.myTites.value = '';
       this.myAuth.value = '';
       eror.textContent = '';
     } else {
-        eror.textContent = 'Please enter both title and author';
+      eror.textContent = 'Please enter both title and author before proceeding';
     }
   }
 
-  removeBook(title) {
-    this.bookstore  = this.bookstore .filter((book) => book.title !== title);
-    this.saveBooks();
-    this.renderBooks();
+  remofBooks(title) {
+    this.bookstore = this.bookstore.filter((book) => book.title !== title);
+    this.saveBookf();
+    this.deliverBooks();
   }
 
-  renderBooks() {
-    this.myList.innerHTML = '';
+  deliverBooks() {
+    this.List.innerHTML = '';
 
-    if (this.bookstore .length === 0) {
-      const emptyMessage = document.createElement('li');
-      emptyMessage.textContent = 'No books in collection';
-      this.myList.appendChild(emptyMessage);
+    if (this.bookstore.length === 0) {
+      const emptyMsge = document.createElement('li');
+      emptyMsge.textContent = 'No books in collection';
+      this.List.appendChild(emptyMsge);
     } else {
-      this.bookstore .forEach((book) => {
-        const li = document.createElement('li');
-        li.innerHTML = `${book.title} by ${book.author}  <br/>`;
-        li.classList = 'list-items';
-        const removeBtn = document.createElement('button');
-        removeBtn.textContent = 'Remove';
-        removeBtn.classList = 'remove-bttn';
-        removeBtn.addEventListener('click', () => this.removeBook(book.title));
-        li.appendChild(removeBtn);
-        this.myList.appendChild(li);
-        this.myList.style.border = '2px solid #000';
+      this.bookstore.forEach((book) => {
+        const lit = document.createElement('lit');
+        lit.innerHTML = `${book.title} by ${book.author}  <br/>`;
+        lit.classList = 'list-items';
+        const removeButtn = document.createElement('button');
+        removeButtn.textContent = 'Remove';
+        removeButtn.classList = 'eliminate-bttn';
+        removeButtn.addEventListener('click', () => this.remofBooks(book.title));
+        lit.appendChild(removeButtn);
+        this.List.appendChild(lit);
+        this.List.style.border = '2px solid #000';
         myText.style.display = 'block';
         const hr = document.createElement('hr');
-        li.appendChild(hr);
-        const myListItems = document.querySelectorAll('.list-items');
-        const myLength = this.myList.childElementCount;
+        lit.appendChild(hr);
+        const ListItems = document.querySelectorAll('.list-items');
+        const myLength = this.List.childElementCount;
         for (let i = 0; i < myLength; i += 2) {
-          myListItems[i].style.backgroundColor = '#dddddd';
+          ListItems[i].style.backgroundColor = '#dddddd';
         }
       });
     }
   }
 
   init() {
-    this.myAdd.addEventListener('click', () => this.addBook());
-    this.renderBooks();
+    this.ourAdd.addEventListener('click', () => this.addBuks());
+    this.deliverBooks();
   }
 }
 
